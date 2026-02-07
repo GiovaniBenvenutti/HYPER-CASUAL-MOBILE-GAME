1cc92888c57b79b72fc08c74e76237e1b33df0f2
GiovaniBenvenutti <giovanibenvenutti2010@hotmail.com>
2026-02-07 15:47:29 -0300

    creat player basics input sistem

--- Assets/_Scripts/TouchController.cs
+++ Assets/_Scripts/TouchController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class TouchController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector2 postPosition;
+    public float velocity = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // ESSE SERIA O INPUT PARA O CELULAR, MAS COMO ESTOU TESTANDO NO PC, VOU USAR O INPUT DO MOUSE
+        // if(Input.GetTouch(0).phase == TouchPhase.Moved)
+        // {
+        //     Move(Input.GetTouch(0).deltaPosition.x);
+        // }
+        if(Input.GetMouseButton(0))
+        {
+           Move(Input.mousePosition.x - postPosition.x);
+        }
+        postPosition = Input.mousePosition;
+    }
+
+    public void Move (float speed) 
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime * velocity;
     }
 }
